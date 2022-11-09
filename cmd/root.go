2e92a26a911e3e1d5fde2975d2aafbb8149833b8
reggiepy <wangtongpy@foxmail.com>
2022-11-09 16:35:42 +0800

    (all)remove print

--- cmd/root.go
+++ cmd/root.go
@@ -11,8 +11,7 @@ import (
 
 var (
 	// Used for flags.
-	cfgFile     string
-	userLicense string
+	cfgFile string
 
 	rootCmd = &cobra.Command{
 		Use:   "win_server_gen",
@@ -31,13 +30,8 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-	rootCmd.PersistentFlags().StringP("author", "a", "reggiepy", "author name for copyright attribution")
-	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
 	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
 	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-	viper.SetDefault("author", "reggiepy <reggiepy@foxmail.com>")
-	viper.SetDefault("license", "apache")
 	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
 }
 
@@ -63,7 +57,6 @@ func initConfig() {
 	}
 
 	viper.AutomaticEnv()
-	fmt.Println(viper.ReadInConfig())
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
